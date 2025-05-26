a2e245d846686b3d14f51f01b722189bcc55b022
MACBOOK PRO 2019 <thanglphce171637@fpt.edu.vn>
2025-05-26 23:12:53 +0700

    no message

--- app.js
+++ app.js
@@ -59,6 +59,12 @@ app.use("/payments", PaymentRouter);
 app.use("/revenue", RevenueRouter);
 app.use("/comments", CommentRouter);
 
+app.use(express.static(path.join(__dirname, "client/build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client/build", "index.html"));
+});
+
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
